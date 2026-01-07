08d3b1daf2d1d9198243f372dd3c31608c3556cf
Emmanuel <blaemedia26@gmail.com>
2026-01-07 07:38:20 +0100

    Cache Property List View

--- alx-backend-caching_property_listings/urls.py
+++ alx-backend-caching_property_listings/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import path, include
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("properties/", include("properties.urls")),
 ]
--- properties/urls.py
+++ properties/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import property_list
 
 urlpatterns = [
-    path("", property_list, name="property-list"),
+    path("", property_list),
 ]
